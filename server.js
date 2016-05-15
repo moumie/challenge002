0100022c871558f83d99c44785cb6c4d322cf98c
soulemane <moumie.soulemane@gmail.com>
2016-05-15 23:45:43 +0200

    mongo and mongoose added

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ var router = express.Router();
 var path = __dirname + '/views/';
 var port = process.env.PORT || 1337;
 
-/*
- we have defined the Router middle layer, which will be executed 
- before any other routes. This route will be used to print the 
- type of HTTP request the particular Route is referring to.
- 
- Once the middle layer is defined, you must pass "next()" so that 
- next router will get executed.
-*/
+//Mongoose models
+var userOp = require("./models/user");
+var docOp = require("./models/doc");
+
+//list of users online
+var userOnlineList = new Object(); 
+
+
 router.use(function (req,res,next) {
   console.log("/" + req.method);
   next();
