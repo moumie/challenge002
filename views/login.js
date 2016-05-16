e9fd41988336e030b67937a31ca7cf59b391b8b2
soulemane <moumie.soulemane@gmail.com>
2016-05-16 09:52:50 +0200

    login script added

--- views/login.js
+++ views/login.js
@@ -1,18 +1,25 @@
-$(function() {
+/* 
+* This example uses the node MongoDB module to connect to the local
+* mongodb database on this virtual machine
+*
+* More here: http://mongodb.github.io/node-mongodb-native/markdown-docs/insert.html
+*/
 
-    $('#login-form-link').click(function(e) {
-		$("#login-form").delay(100).fadeIn(100);
- 		$("#register-form").fadeOut(100);
-		$('#register-form-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
-	$('#register-form-link').click(function(e) {
-		$("#register-form").delay(100).fadeIn(100);
- 		$("#login-form").fadeOut(100);
-		$('#login-form-link').removeClass('active');
-		$(this).addClass('active');
-		e.preventDefault();
-	});
+//require node modules (see package.json)
+var MongoClient = require('mongodb').MongoClient
+    , format = require('util').format;
 
+//connect away
+MongoClient.connect('mongodb://moumie23:moumie23@ds011442.mlab.com:11442/moumiemongodb', function(err, db) {
+  if (err) throw err;
+  console.log("Connected to Database");
+
+  //simple json record
+	var document = {name:"David", title:"About MongoDB"};
+  
+	//insert record
+	db.collection('test').insert(document, function(err, records) {
+		if (err) throw err;
+		console.log("Record added as "+records[0]._id);
+	});
 });
